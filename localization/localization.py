c2f944e64e13cd90adb0237c6c67e81c8c5b0049
Federico De Santi <federico.ds25@gmail.com>
2023-05-21 18:43:52 +0200

    aggiunta feature per visualizzare tutte le misure

--- localization/localization.py
+++ localization/localization.py
@@ -2,8 +2,10 @@ from tkinter import *
 import scipy.io
 import json
 import os
+import math
 
-########## FUNCTIONS
+
+########## UTILITY FUNCTIONS
 
 #counts the number of directories (useful to know how many measurement are present in the big folder passed through the setup_localization.conf json)
 def count_directories(directory):
@@ -33,21 +35,60 @@ def draw_circle(canvas, center_x, center_y, radius, colour):
     x2 = center_x + radius
     y2 = center_y + radius
     canvas.create_oval(x1, y1, x2, y2, fill=colour)
-    
-    
+
+
 ##########CONSTANTS (lenghts are in centimiters)
 ROOM_WIDTH = 237
 ROOM_HEIGHT = 428
-PADDING = 20                                #border outside the room drawing
+PADDING = 20*10                                #border outside the room drawing
 CANVAS_WIDTH = ROOM_WIDTH + PADDING*2
 CANVAS_HEIGHT = ROOM_HEIGHT + PADDING*2
 TX_X = 119                                  #x position of the ap
 TX_Y = 5                                    #y position of the ap
 ANGLE_ERROR = 8                          #maximum +- error in the azimuth angle estimate
-RX_TILT = get_param_from_json_file ("rx_tilt", "/home/des/Documents/università/tesi_onde_millimetriche/localization/setup_localization.conf")  #how much (in degrees) the client is tilted in respect to the line that passes through the TX and the RX
+RX_TILT = get_param_from_json_file ("rx_tilt", "setup_localization.conf")  #how much (in degrees) the client is tilted in respect to the line that passes through the TX and the RX
 DIRECTORY_BATCH_MEASURES = get_param_from_json_file("directory_batch_measures", "setup_localization.conf") #the folder containing all the measurements
 NUM_MEASURES = count_directories(DIRECTORY_BATCH_MEASURES)
+MAINPATH_FILE_PATH = "/processed_data/md_track/mainPathInfo.txt"
 
+
+
+#function that gets the main path info from all the measurements
+def import_main_paths_info(az_angles, el_angles, pow_values, dist_values):
+    for i in range(1, NUM_MEASURES+1):
+        az_angles.append(get_param_from_json_file("azimuthAngle", DIRECTORY_BATCH_MEASURES + "/v" + str(i) + MAINPATH_FILE_PATH))
+        el_angles.append(get_param_from_json_file("elevationAngle", DIRECTORY_BATCH_MEASURES + "/v" + str(i) + MAINPATH_FILE_PATH))
+        pow_values.append(get_param_from_json_file("power", DIRECTORY_BATCH_MEASURES + "/v" + str(i) + MAINPATH_FILE_PATH))
+        dist_values.append(get_param_from_json_file("distance", DIRECTORY_BATCH_MEASURES + "/v" + str(i) + MAINPATH_FILE_PATH))
+
+#try to estimate the client position based on the line of sight
+def get_estimated_client_position(az_angles, el_angles, dist_values):
+    average_distance = 0
+    average_azimuth_angle = 0
+    num_measurements = 0
+
+    print("per la stima del client vado a prendere il main path delle misure ")
+    for i in range(0, NUM_MEASURES):
+        if((az_angles[i] + RX_TILT) in range(-ANGLE_ERROR, ANGLE_ERROR) and el_angles[i] in range(-ANGLE_ERROR, ANGLE_ERROR)): 
+            print("v" + str(i+1))
+            average_distance += dist_values[i]
+            average_azimuth_angle += az_angles[i]
+            num_measurements += 1
+
+    #keep in mind that the distance is in meters, so i have to convert it in centimiters (*100)
+    return (100 * (average_distance / num_measurements)), (average_azimuth_angle / num_measurements)
+
+#function that draws every measure
+def draw_measures(az_angles, dist_values):
+    for i in range(0, NUM_MEASURES):
+        #calculate the x and y coordinates of the measure
+        x = TX_X + (dist_values[i]*100 * math.sin(math.radians(az_angles[i])))
+        y = TX_Y + (dist_values[i]*100 * math.cos(math.radians(az_angles[i])))
+        print("x: " + str(x) + " y: " + str(y) + " dist: " + str(dist_values[i]*100) + " az: " + str(az_angles[i]))
+
+        draw_circle(myCanvas, x+PADDING, y+PADDING, 2, "orange")
+        
+    
 
 #import matlab processed files
 #servirà in futuro, quando dovrò guardare dati oltre al main path per ogni misura
@@ -61,31 +102,15 @@ azimuth_angles = []
 elevation_angles = []
 power_values = []
 distances = []
-for i in range(1, NUM_MEASURES+1):
-    azimuth_angles.append(get_param_from_json_file("azimuthAngle", DIRECTORY_BATCH_MEASURES + "/v" + str(i) + "/processed_data/md_track/mainPathInfo.txt"))
-    elevation_angles.append(get_param_from_json_file("elevationAngle", DIRECTORY_BATCH_MEASURES + "/v" + str(i) + "/processed_data/md_track/mainPathInfo.txt"))
-    power_values.append(get_param_from_json_file("power", DIRECTORY_BATCH_MEASURES + "/v" + str(i) + "/processed_data/md_track/mainPathInfo.txt"))
-    distances.append(get_param_from_json_file("distance", DIRECTORY_BATCH_MEASURES + "/v" + str(i) + "/processed_data/md_track/mainPathInfo.txt"))
+import_main_paths_info(azimuth_angles, elevation_angles, power_values, distances)
 
 #try to estimate the client position based on the line of sight
-average_distance = 0
-average_azimuth_angle = 0
-num_measurements = 0
-print("per la stima del client vado a prendere il main path delle misure ")
-for i in range(0, NUM_MEASURES):
-    if((azimuth_angles[i] + RX_TILT) in range(-ANGLE_ERROR, ANGLE_ERROR) and elevation_angles[i] in range(-ANGLE_ERROR, ANGLE_ERROR)):        
-        print("v" + str(i+1))
-        average_distance += distances[i]
-        average_azimuth_angle += azimuth_angles[i]
-        num_measurements += 1
-average_distance = 100 * (average_distance / num_measurements)
-average_azimuth_angle = average_azimuth_angle / num_measurements
-
+average_distance, average_azimuth_angle = get_estimated_client_position(azimuth_angles, elevation_angles, distances)
 
 #canvas setup
 root = Tk()
 root.title("Client's aproximate position")
-root.geometry("500x500")
+root.geometry(str(CANVAS_WIDTH) + "x" + str(CANVAS_HEIGHT))
 
 myCanvas = Canvas(root, width=CANVAS_WIDTH, height=CANVAS_HEIGHT, bg="white")
 myCanvas.pack(pady=0)
@@ -98,8 +123,10 @@ myCanvas.create_rectangle(0+PADDING, 0+PADDING, ROOM_WIDTH+PADDING, ROOM_HEIGHT+
 draw_circle(myCanvas, TX_X+PADDING, TX_Y+PADDING, 3, "green")
 
 #draw RX estimated position
-draw_circle(myCanvas, TX_X+PADDING, TX_Y+average_distance+PADDING, 5, "red")
+#draw_circle(myCanvas, TX_X+PADDING, TX_Y+average_distance+PADDING, 5, "red")
 
+#draw measures
+draw_measures(azimuth_angles, distances)
 
 #end draw statement
 root.mainloop()
