243cbd7e0a8d86ba01eb041944c6ad022d8e4c56
Federico De Santi <federico.ds25@gmail.com>
2023-07-28 16:08:56 +0200

    little fixes

--- localization/trilateration_3d/trilateration.py
+++ localization/trilateration_3d/trilateration.py
@@ -3,6 +3,7 @@ from scipy.optimize import minimize
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
+#----------------------------------CLASS----------------------------------
 class Sphere:
     def __init__(self, x, y, z, radius):
         self.x = x
@@ -10,11 +11,14 @@ class Sphere:
         self.z = z
         self.radius = radius
 
+#----------------------------------FUNCTIONS----------------------------------
+
 # Funzione di distanza tra un punto e una sfera
 def distance_from_point_to_sphere(point, sphere):
     center = np.array([sphere.x, sphere.y, sphere.z])
     dist_squared = abs(np.linalg.norm(point - center) - sphere.radius) ** 2
     return dist_squared
+
 
 # Funzione di distanza tra un punto e un insieme di sfere
 def distance_from_point_to_spheres(point, spheres):
@@ -24,6 +28,7 @@ def distance_from_point_to_spheres(point, spheres):
         total_distance_squared += dist_squared
     return total_distance_squared
 
+
 # Funzione di ottimizzazione
 def optimize_distance(spheres):
     result = minimize(
@@ -32,6 +37,8 @@ def optimize_distance(spheres):
         method='BFGS',  # Metodo di ottimizzazione
     )
     return result.x
+
+#----------------------------------MAIN----------------------------------
 
 # Definizione delle sfere
 ap_height = 2.44
@@ -44,21 +51,23 @@ spheres = [
 
 # Calcolo del punto più probabile di intersezione delle sfere
 intersection_point = optimize_distance(spheres)
-print("Punto più probabile di intersezione:", intersection_point)
+print("Estimated point:", intersection_point)
+print("Real point: [4.44, 2, 0.93]")
 
 
-#stampo la distanza tra ogni sfera e il punto (4.44, 2, 0.93)
-for sphere in spheres:
-    print("Distanza tra il punto e la sfera: ", distance_from_point_to_sphere(np.array([4.44, 2, 0.93]), sphere))
+#print point to point distance between the estimated point and (4.44, 2, 0.93)
+print("Distanza tra il punto stimato e la ground truth: ", np.linalg.norm(intersection_point - np.array([4.44, 2, 0.93])))
 
 
-# Creazione del grafico 3D
+#----------------------------------PLOT----------------------------------
+
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 
 # Plot dei centri delle sfere come tondini blu
 for sphere in spheres:
     ax.scatter(sphere.x, sphere.y, sphere.z, c='blue', marker='o')
+
 
 ax.scatter (4.44, 2, 0.93, c='green', marker='o')
 # Plot del punto stimato come "x" rossa
