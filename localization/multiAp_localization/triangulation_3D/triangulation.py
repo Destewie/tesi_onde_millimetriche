ca999bdc4080099de69086c560cf7a676f7f22c0
Federico De Santi <federico.ds25@gmail.com>
2023-08-01 01:50:38 +0200

    primo fallimento nella stima del punto con la triangolazione

--- localization/multiAp_localization/triangulation_3D/triangulation.py
+++ localization/multiAp_localization/triangulation_3D/triangulation.py
@@ -3,7 +3,9 @@ import os
 import json
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+from scipy.optimize import minimize
 import numpy as np
+import math
 
 #----------------------------------CONSTANTS---------------------------------
 # costante per un threshold sotto il quale il raggio non viene considerato
@@ -16,8 +18,6 @@ ANGLE_OFFSET = 90
 X_ROOM = 6.442
 Y_ROOM = 7.245
 Z_ROOM = 2.64
-
-
 
 #----------------------------------CLASSES---------------------------------
 
@@ -42,7 +42,7 @@ class Client:
 class Ray:
     def __init__(self, id, azimuth, elevation, power):
         self.id = id #ogni raggio appartiene ad un router
-        self.azimuth = azimuth
+        self.azimuth = azimuth 
         self.elevation = elevation
         self.power = power #lo userò come peso per il calcolo delle coordinate
 
@@ -98,6 +98,63 @@ def get_rays(measures_file):
     return rays
 
 
+# Funzione per la distanza router - raggio con lo stesso id (va usata una funzione di distanza per punto - retta, in cui il segmento che identifica la distanza è sempre perpendicolare alla retta)
+def distance_router_ray(router, starting_point, ending_point):
+    point = np.array([router.x, router.y, router.z])
+
+    line_direction = ending_point - starting_point
+    point_to_line_start = point - starting_point
+
+    cross_product = np.cross(line_direction, point_to_line_start)
+    numerator = np.linalg.norm(cross_product)
+    denominator = np.linalg.norm(line_direction)
+
+    return numerator / denominator
+
+# Funzione da minimizzare in cui sommo tutte le distanze tra i router e i raggi con lo stesso id
+def error_calculation(client, rays, routers):
+    #i raggi partono dalla posizione del client
+    starting_point = np.array([client[0], client[1], client[2]]) #rispettivamente x, y, z
+    line_length = 10 #la lunghezza è ininfluente ai fini del calcolo dell'errore
+
+    error = 0
+
+    #calcolo l'ending point
+    for ray in rays:
+        #se il raggio ha un power troppo basso non lo considero
+        if(ray.power > POWER_THRESHOLD):
+            #converto gli angoli in radianti
+            azimuth = np.radians(-ray.azimuth + client[3]) #devo sottrarre un alpha (client[3]) per ruotare i raggi. Ricorda che l'algoritmo di minimizzazione avrà da lavorare anche con l'inclinazione del client e non solo con la sua posizione spaziale
+            elevation = np.radians(ray.elevation)
+
+            #calcolo l'ending point
+            ending_point = starting_point + line_length * np.array([np.cos(azimuth) * np.cos(elevation), 
+                                                                    np.sin(azimuth) * np.cos(elevation), 
+                                                                    np.sin(elevation)]) 
+
+            #calcolo la distanza tra il router e il raggio con lo stesso id
+            for router in routers:
+                if(router.id == ray.id):
+                    error += distance_router_ray(router, starting_point, ending_point)
+                    break
+
+    return error
+
+# Funzione che minimizza l'errore calcolato e ritorna le coordinate del punto in cui l'errore è minimo
+def minimize_error(client, rays, routers):
+    #inizializzo il punto di partenza
+    starting_point = np.array([client.x, client.y, client.z, client.alpha])
+
+    #minimizzo l'errore
+    res = minimize(error_calculation, starting_point, args=(rays, routers), method='BFGS')
+
+    #recupero le coordinate del punto in cui l'errore è minimo
+    x = res.x[0]
+    y = res.x[1]
+    z = res.x[2]
+    alpha = res.x[3]
+
+    return x, y, z, alpha
 
 
 #----------------------------------MAIN---------------------------------
@@ -115,6 +172,17 @@ routers = get_routers(sys.argv[2])
 rays = get_rays(sys.argv[1])
 
 
+#minimizzo l'errore
+client = Client(0, 0, 0, 0)
+x, y, z, alpha = minimize_error(client, rays, routers)
+
+#stampo le coordinate del punto in cui l'errore è minimo
+print("Coordinate: " + str(x) + " " + str(y) + " " + str(z))
+
+#stampo l'errore minimo inteso come la distanza tra il punto in cui l'errore è minimo e il punto in cui si trova il client
+print("Errore: " + str(np.linalg.norm(np.array([x, y, z]) - np.array([real_client_coordinates["x"], real_client_coordinates["y"], real_client_coordinates["z"]]))))
+
+
 
 #----------------------------------3D PLOT---------------------------------
 
@@ -126,12 +194,11 @@ ax = fig.add_subplot(111, projection='3d')
 for r in routers:
     ax.scatter(r.x, r.y, r.z, c='blue', marker='o')
 
-#plotto i raggi partendo dal client
+#plotto i veri raggi derivanti dalle misure partendo dal client
 start_point = np.array([real_client_coordinates["x"], real_client_coordinates["y"], real_client_coordinates["z"]])
-
 for ray in rays:
     #converto angoli
-    azimuth_rad = np.radians(-ray.azimuth - real_client_coordinates["tilt"] + ANGLE_OFFSET)
+    azimuth_rad = np.radians(-ray.azimuth - real_client_coordinates["tilt"] + ANGLE_OFFSET) #tocca fare un po' di magheggi se voglio che il plot rispecchi la realtà
     elevation_rad = np.radians(ray.elevation)
 
     line_length = 15 
@@ -141,11 +208,14 @@ for ray in rays:
                                                       np.sin(azimuth_rad) * np.cos(elevation_rad), 
                                                       np.sin(elevation_rad)]) 
 
-    #plotto il raggio
-    ax.plot([start_point[0], end_point[0]], [start_point[1], end_point[1]], [start_point[2], end_point[2]], c='red')
+    #plotto il raggio con spessore in base alla power
+    ax.plot([start_point[0], end_point[0]], [start_point[1], end_point[1]], [start_point[2], end_point[2]], c='green', linewidth=3*ray.power)
 
-#plotto il client
+#plotto il vero client
 ax.scatter(real_client_coordinates["x"], real_client_coordinates["y"], real_client_coordinates["z"], c='green', marker='o')
+
+#plotto il client stimato
+ax.scatter(x, y, z, c='red', marker='x')
 
 # Opzionale: Aggiungi etichette agli assi
 ax.set_xlabel('X')
@@ -153,9 +223,9 @@ ax.set_ylabel('Y')
 ax.set_zlabel('Z')
 
 # Opzionale: Imposta i limiti degli assi
-ax.set_xlim([0, X_ROOM])  # Sostituisci xmin e xmax con i limiti dell'asse X
-ax.set_ylim([0, Y_ROOM])  # Sostituisci ymin e ymax con i limiti dell'asse Y
-ax.set_zlim([0, Z_ROOM])  # Sostituisci zmin e zmax con i limiti dell'asse Z
+ax.set_xlim([0, X_ROOM])  
+ax.set_ylim([0, Y_ROOM]) 
+ax.set_zlim([0, Z_ROOM]) 
 
 
 plt.show()
