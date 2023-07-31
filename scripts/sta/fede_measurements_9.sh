6b2ab7a9da0f5ba0e1fb76c4aabca5bd3763a172
Federico De Santi <federico.ds25@gmail.com>
2023-07-31 02:39:40 +0200

    aggiunti diversi file precompilati per collegarsi ai router

--- scripts/sta/fede_measurements_9.sh
+++ scripts/sta/fede_measurements_9.sh
@@ -2,9 +2,9 @@
 
 
 echo "Starting with the csi measures..."
-~/Experiments/csi300_v3.sh /tmp/csi_measurements_fede.txt 'Mikrotik' '\x08\x55\x31\x70\xFF\x0A'
+~/Experiments/csi300_v3.sh /tmp/csi_measurements_fede.txt 'Mikrotik9' '\x08\x55\x31\x9B\x09\xF4'
 echo "csi measures done!"
 
 echo "Starting with the ftm measures..."
-~/Experiments/ftm10.sh /tmp/ftm_measurements_fede.txt 'Mikrotik' '\x08\x55\x31\x70\xFF\x0A'
+~/Experiments/ftm10.sh /tmp/ftm_measurements_fede.txt 'Mikrotik9' '\x08\x55\x31\x9B\x09\xF4'
 echo "ftm measures done!"
