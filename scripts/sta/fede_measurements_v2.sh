29d5e6b8b69a9f643ea115059de6eb835a754b63
Federico De Santi <federico.ds25@gmail.com>
2023-08-02 19:31:56 +0200

    modificato fede_measurements_v2.sh

--- scripts/sta/fede_measurements_v2.sh
+++ scripts/sta/fede_measurements_v2.sh
@@ -1,27 +1,56 @@
 
-if [ $# -ne 2 ]; then
-    echo "Usage: $0 <device_name> <mac_address>"
-    echo "Example: $0 Mikrotik '\x08\x55\x31\x70\xFF\x0A'"
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <id del router a cui vuoi connetterti>"
+    echo "Example: $0 12"
     exit 1
 fi
 
-#imposto l'SSID adeguato 
-ssid="Mikrotik$2"
+ssid="Mikrotik$1"
 
+router_found=false
 
-#se il secondo parametro è uguale a 9, allora l'SSID diventa Mikrotik9 e il MAC address diventa 08:55:31:70:FF:09
-if [ $2 -eq 9 ]; then
-    mac_address="\x08\x55\x31\x70\xFF\x09"
-else
+if [ "$1" -eq "9" ]
+then
+    mac_address="\x08\x55\x31\x9B\x09\xF4"
+        router_found=true
+fi
+if [ "$1" -eq "10" ]
+then
+    mac_address="\x08\x55\x31\x9B\x0A\x1B"
+        router_found=true
+fi
+if [ "$1" -eq "11" ]
+then
+    mac_address="\x08\x55\x31\x9B\x0A\x1D"
+        router_found=true
+fi
+if [ "$1" -eq "12" ]
+then
+    mac_address="\x08\x55\x31\x9B\x05\x29"
+        router_found=true
+fi
+if [ "$1" -eq "13" ]
+then
+    mac_address="\x08\x55\x31\x9B\x04\xAC"
+        router_found=true
+fi
 
+if ! $router_found
+then
+        echo "Non ho trovato un match per il numero che hai inserito"
+        exit 0
+fi
 
-command="~/Experiments/csi300_v3.sh /tmp/csi_measurements_fede.txt $device_name '$mac_address'"
+command="~/Experiments/csi300_v3.sh /tmp/csi_measurements_fede.txt $ssid '$mac_address'"
+echo $command
 echo "Starting with the csi measures..."
 eval "$command"
 echo "csi measures done!"
 
-command="~/Experiments/csi300_v3.sh /tmp/ftm_measurements_fede.txt $device_name '$mac_address'"
+command="~/Experiments/csi300_v3.sh /tmp/ftm_measurements_fede.txt $ssid '$mac_address'"
+echo $command
 echo "Starting with the ftm measures..."
 eval "$command"
 echo "ftm measures done!"
+
 
