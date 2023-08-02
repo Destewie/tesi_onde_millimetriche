66e8e4dd0dcdb5d0bb8a9ed94e1298368abc2199
Federico De Santi <federico.ds25@gmail.com>
2023-08-02 16:11:08 +0200

    fatto il plot della triangolazione con i raggi che partono dai router

--- localization/multiAp_localization/triangulation_3D/triangulation_rays_from_routers.py
+++ localization/multiAp_localization/triangulation_3D/triangulation_rays_from_routers.py
@@ -16,6 +16,9 @@ POWER_THRESHOLD = 0.25
 #angolo in gradi per ruotare sempre i raggi in modo che gli 0 gradi siano paralleli all'asse Y
 ANGLE_OFFSET = 90
 
+#lunghezza fittizia dei raggi che disegno
+RAY_LENGTH = 20
+
 # Dimensione della stanza
 X_ROOM = 6.442
 Y_ROOM = 7.245
@@ -52,9 +55,9 @@ class Router:
         azimuth_diff_ap_client = differenza_angoli(base_angle_for_router, self.client_tilt)
         azimuth_rad = np.radians(base_angle_for_router + azimuth_diff_ap_client - self.ray.azimuth)
         elevation_rad = np.radians(-self.ray.elevation)
-        x = self.x + np.cos(azimuth_rad) * np.cos(elevation_rad)
-        y = self.y + np.sin(azimuth_rad) * np.cos(elevation_rad)
-        z = self.z + np.sin(elevation_rad)
+        x = self.x + RAY_LENGTH * np.cos(azimuth_rad) * np.cos(elevation_rad)
+        y = self.y + RAY_LENGTH * np.sin(azimuth_rad) * np.cos(elevation_rad)
+        z = self.z + RAY_LENGTH * np.sin(elevation_rad)
         return np.array([x, y, z])
 
     #----------------------------------FUNCTIONS--------------------------------------
@@ -94,7 +97,7 @@ def get_rays(measures_file):
     return rays
 
 # Funzione che crea la lista dei router
-def get_routers(routers_file, rays):
+def get_routers(routers_file, rays, client_info):
     with open(routers_file) as f:
         routers_from_json = json.load(f)
 
@@ -103,10 +106,104 @@ def get_routers(routers_file, rays):
     for key in keys:
         for ray in rays:
             if ray.id == key:
-                r = Router(key, routers_from_json[key]["x"], routers_from_json[key]["y"], routers_from_json[key]["height"], routers_from_json[key]["tilt"], real_client_coordinates["tilt"], ray)
+                r = Router(key, routers_from_json[key]["x"], routers_from_json[key]["y"], routers_from_json[key]["height"], routers_from_json[key]["tilt"], client_info["tilt"], ray)
                 routers.append(r)
 
     return routers
 
+#ritorna il sottoinsieme di router di qulità
+def get_reliable_routers(routers):
+    reliable_routers = []
+    for router in routers:
+        if quality_test_router(router):
+            reliable_routers.append(router)
+    return reliable_routers
 
+
+# Funzione del quality test di un router
+def quality_test_router(router):
+    return (router.ray.power >= POWER_THRESHOLD)
+
+
+# Funzione importantissima che mi serve per capire di quanti gradi dovrei tiltare l'ap per avere che punta in modo parallelo ed opposto alla direzione del client
+def differenza_angoli(angolo1, angolo2):
+    # Converto gli angoli in radianti
+    rad_angolo1 = math.radians(angolo1)
+    rad_angolo2 = math.radians(angolo2)
+    
+    # Calcolo la differenza in radianti utilizzando atan2
+    diff_rad = math.atan2(math.sin(rad_angolo1 - rad_angolo2), math.cos(rad_angolo1 - rad_angolo2))
+    
+    # Converto la differenza in gradi
+    diff_gradi = math.degrees(diff_rad)
+    
+    # Assicuriamoci che il risultato sia compreso tra -179 e 180 gradi
+    if diff_gradi > 180:
+        diff_gradi -= 360
+    elif diff_gradi < -180:
+        diff_gradi += 360
+    
+    return -diff_gradi
+
+
+
+
+#----------------------------------MAIN----------------------------------
+
+#controllo argomenti
+check_args()
+
+#do un nome ai file passati in input
+measures_file = sys.argv[1]
+routers_file = sys.argv[2]
+
+#prendo le coordinate reali del client
+real_client_coordinates = get_real_client_position(measures_file)
+
+#prendo i raggi
+rays = get_rays(measures_file)
+
+#prendo i router
+routers = get_routers(routers_file, rays, real_client_coordinates)
+
+print()
+
+#printo la vera posizione del client
+print("Vera posizione del client: " + str(real_client_coordinates))
+
+
+
+#----------------------------------3D PLOT----------------------------------
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+#plotto ogni router e il suo raggio
+for router in routers:
+    #plotto il raggio
+    x = [router.x, router.get_ray_end_point()[0]]
+    y = [router.y, router.get_ray_end_point()[1]]
+    z = [router.z, router.get_ray_end_point()[2]]
+    ax.plot(x, y, z, color='b', linewidth=3*router.ray.power)
+    
+    #plotto il router (grosso)
+    ax.scatter(router.x, router.y, router.z, color='b', s=100)
+
+#plotto la vera posizione del client
+ax.scatter(real_client_coordinates["x"], real_client_coordinates["y"], real_client_coordinates["z"], color='green')
+
+# Opzionale: Aggiungi etichette agli assi
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+ax.set_zlabel('Z')
+
+# Opzionale: Imposta i limiti degli assi
+ax.set_xlim([0, X_ROOM])  
+ax.set_ylim([0, Y_ROOM]) 
+ax.set_zlim([0, Z_ROOM]) 
+
+
+plt.show()
+
+#----------------------------------END----------------------------------
 
