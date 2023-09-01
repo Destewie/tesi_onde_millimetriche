5817a1ed9a22cb50e32c7a9f6cb6a293bb7b018c
Federico De Santi <federico.ds25@gmail.com>
2023-09-01 14:28:29 +0200

    fatto plot per gli errori medi a confronto

--- localization/multiAp_localization/algorithm_error_visualization/error_visualization_per_folder/average_error_every_approach_per_folder.py
+++ localization/multiAp_localization/algorithm_error_visualization/error_visualization_per_folder/average_error_every_approach_per_folder.py
@@ -1,7 +1,8 @@
 import os
 import sys
+import matplotlib.pyplot as plt
 
-#-------------------------------------------------------------
+#----------------------------- FUNZIONI --------------------------------
 
 # Funzione che controlla che ci sia un parametro in ingresso e che questo sia una cartella
 def check_input():
@@ -18,7 +19,10 @@ def get_average_error_per_method_from_files():
 
     for dir_misura in os.listdir(sys.argv[1]):
         full_path_dir_misura = os.path.join(sys.argv[1], dir_misura)
-        if(os.path.isdir(full_path_dir_misura)):
+        if(not os.path.isdir(full_path_dir_misura)):
+            #solleva eccezione se non è una cartella
+            raise Exception("ERROR: il parametro inserito non è una cartella")
+        else :
             for file in os.listdir(full_path_dir_misura):
                 if file.endswith(".txt"):
                     #prendo la parte prima del .txt e, se non esiste, la creo con valore 0
@@ -29,6 +33,11 @@ def get_average_error_per_method_from_files():
                     errore_da_file = float(open(path_file_errore, "r").read())
                     error_per_localization_method[nome_file] = valore_nel_dizionario + errore_da_file
 
+    #se il dict è rimasto vuoto sollevo un'eccezione
+    if not error_per_localization_method:
+        raise Exception("ERROR: non ci sono file .txt nella cartella di misure")
+
+
     numero_di_misure = len(os.listdir(sys.argv[1]))
 
     #calcolo la media per ogni errore accumulato finora
@@ -37,11 +46,40 @@ def get_average_error_per_method_from_files():
 
     return error_per_localization_method
 
-#-------------------------------------------------------------
+
+def ottieni_plot(errori_medi_dict):
+    
+    #assegno un colore ad ogni barra
+    colori = ['b', 'm', 'r', 'c', 'm', 'y', 'k', 'w']
+
+    plt.bar(range(len(errori_medi_dict)), list(errori_medi_dict.values()), align='center', color=colori)
+    plt.xticks(range(len(errori_medi_dict)), list(errori_medi_dict.keys()))
+    plt.ylabel("Errore medio")
+    plt.title("Errore di localizzazione medio per ogni approccio")
+
+    return plt
+    
+#salva il plot nella stessa cartella passata come parametro come un pdf
+def salva_plot(plot):
+    nome_file = "plot_errore_medio.pdf"
+    path_file = os.path.join(sys.argv[1], nome_file)
+    plot.savefig(path_file, bbox_inches='tight')
+
+
+#----------------------------- MAIN --------------------------------
 
 if __name__ == "__main__":
     check_input()
-    error_per_localization_method = get_average_error_per_method_from_files()
-    print(error_per_localization_method)
 
+    try:
+        error_per_localization_method = get_average_error_per_method_from_files()
+        print(error_per_localization_method)
+
+        plot_errore_medio = ottieni_plot(error_per_localization_method)
+        salva_plot(plot_errore_medio)
+        plot_errore_medio.show()
+
+    except Exception as e:
+        print(e)
+        exit()
 
